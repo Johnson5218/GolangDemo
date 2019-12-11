0cca3447422857428b5d1638754173cd5d4b41f2
zhang xin <zhangxin@luojilab.com>
2019-12-11 15:48:07 +0800

    test rand.go

---
+++ rand.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+import "math/rand"
+
+
+func main() {
+	r := rand.New(rand.NewSource(99))
+	var temp = r.Int31()
+	for i := 0; i < 20; i++ {
+		temp = r.Int31()
+		fmt.Println(temp)
+	}
+
+}
